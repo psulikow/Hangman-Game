4d2cd63da30140313f5b14f713a9efe7b8f3a587
Paul Sulikowski <paulsulikowski@7BC.local>
2017-02-10 17:10:10 -0600

    need to figure out how to continue if guess is same as last or previous

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,76 @@
 
 
+
+function gameOn() {
+
+    var word = words[Math.floor(Math.random() * words.length)];
+
+    document.onkeyup = function(event) {
+
+            guess++;
+            guessRem--;
+
+
+
+            if (guessRem == 0) {
+                alert('game over');
+            }
+    
+    
+            else {
+                // Determines which key was pressed
+                var userGuess = event.key;
+
+                //filling in guesses remaining
+                var div = document.getElementById('guess-rem');
+                div.innerHTML = guessRem;
+
+                //filling in user guesses
+                var div = document.getElementById('guesses');
+                div.innerHTML += userGuess + ', ';
+
+                // Alerts the key the user pressed (userGuess).
+                console.log(userGuess);
+                console.log(guess);
+                console.log(guessRem);
+
+
+
+            }
+
+
+            
+
+
+            // // Randomly chooses a choice from the options array. This is the Computer's guess.
+            // var computerGuess = options[Math.floor(Math.random() * options.length)];
+
+            // // Alerts the Computer's guess.
+            // alert(computerGuess);
+        
+    };
+
+};
+
+
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var words = ["jerry", "kramer", "george", "elaine"]
 
-// This function is run whenever the user presses a key.
-document.onkeyup = function(start) {
+var guess = 0;
 
-// Determines which key was pressed
-var start = event.key;
+var guessRem = 15;
 
-alert(start);
-};
 
-if (start)
 
-{
+//start game on key press
+document.onkeyup = function() {
 
-alert("game on");
-
-document.onkeyup = function(event) {
-
-// Determines which key was pressed
-var userGuess = event.key;
-
-// Alerts the key the user pressed (userGuess).
-console(userGuess);
-
-// Randomly chooses a choice from the options array. This is the Computer's guess.
-var computerGuess = options[Math.floor(Math.random() * options.length)];
-
-// Alerts the Computer's guess.
-alert(computerGuess);
+gameOn();
 
 };
 
-//randomly pick a word
-var randomWord = words[Math.floor(Math.random() * words.length)];
 
-};
+
+
+ 
